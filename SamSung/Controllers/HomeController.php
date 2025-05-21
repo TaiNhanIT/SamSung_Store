ed4dcf37f318279027a8814e31f358513b24294d
TaiNhanIT <nguyenhuutainhanit@gmail.com>
2025-05-21 13:25:33 +0700

    Delete SamSung directory

--- SamSung/Controllers/HomeController.php
+++
@@ -1,7 +0,0 @@
-<?php
-class HomeController {
-    // Hàm này sẽ hiển thị trang chủ
-    public function index() {
-        require_once __DIR__ . '/../views/home/home.php'; // Nạp view trang chủ
-    }
-}
